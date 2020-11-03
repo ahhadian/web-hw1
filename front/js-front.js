11334fddd95fa007d7af6b11726dcb9f63a51526
kasraabdollahi <kasra.abdolahi1379@gmail.com>
2020-11-03 15:02:10 +0000

    front code merged and debugged (localhost origin fixed and sha256 methods cleaned)

--- front/js-front.js
+++ front/js-front.js
@@ -1,45 +1,47 @@
-var origin = "http://127.0.0.1/";
 function displayRes(result) {
     document.getElementById("sha256_result").innerHTML = result;
 }
-function sendSha256Go(go_or_node) {
+function sendSha256Go(){
     var n1 = document.getElementById("number1").value
     var n2 = document.getElementById("number2").value
     var http = new XMLHttpRequest();
     var params = 'number1=' + n1 + "&number2=" + n2;
-    http.open('POST', origin + go_or_node + "/sha256/", true);
-    if (go_or_node === "go") {
-        http.onreadystatechange = function () {//Call a function when the state changes.
-            if (http.readyState == 4 && http.status == 200) {
+    http.open('POST', "/go/sha256/", true);
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4){
+            if(http.status == 200){
+                displayRes(JSON.parse(http.responseText)["sha256"]);
+            }else{
                 displayRes(http.responseText);
             }
         }
-        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        http.send(params);
     }
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send(params);
 }
-function sendSha256Nodejs() {
+function sendSha256Nodejs(){
     var n1 = document.getElementById("number1").value
     var n2 = document.getElementById("number2").value
-    const data = { "number1": n1, "number2": n2 }
-    fetch(origin + "nodejs/sha256", {
+    const data = {"number1": n1, "number2": n2}
+    fetch("/nodejs/sha256", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
     })
-        .then(response => response.json())
-        .then(data => {
-            displayRes(response)
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+    .then(response => response.json())
+    .then(data => {
+        displayRes(data["sha256"])
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        displayRes("An error occured")
+        console.error('Error:', error);
+    });
+  }
 function onFetchNode() {
-    fetch(origin + 'nodejs/write?input=' + document.getElementById("lineNumber").value)
+    fetch('/nodejs/write?input=' + document.getElementById("lineNumber").value)
         .then(res => {
             if (res.status !== 200) {
                 console.log('Looks like there was a problem. Status Code: ' + response.status);
@@ -53,7 +55,7 @@ function onFetchNode() {
         });
 }
 function onFetchGo() {
-    fetch(origin + 'go/write?input=' + document.getElementById("lineNumber").value)
+    fetch('/go/write?input=' + document.getElementById("lineNumber").value)
         .then(res => {
             console.log("here")
             if (res.status !== 200) {
--- nodejs/index.js
+++ nodejs/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.get("/write", (req, res) => {
+app.get("/nodejs/write", (req, res) => {
     let input = req.query.input;
     let answer;
     if (input === null) {
